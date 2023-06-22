d6d940dfb6efb26957398c51d44f7d5cb0ce232e
YBD21 <santosh.deuja2000@gmail.com>
2023-06-22 10:56:49 +0545

    implement file structure

---
+++ Client-App/src/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+import SignIn from "../features/auth/components/SignIn";
+
+const LoginPage = () => {
+  return <SignIn />;
+};
+
+export default LoginPage;
---
+++ Client-App/src/pages/SignUpPage.jsx
@@ -0,0 +1,7 @@
+import CreateAccount from "../features/auth/components/CreateAccount";
+
+const SignUpPage = () => {
+  return <CreateAccount />;
+};
+
+export default SignUpPage;
