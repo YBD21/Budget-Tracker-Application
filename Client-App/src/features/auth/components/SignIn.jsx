3b39d5ec7a8610df5ab6fb312bc3419ed152b919
YBD21 <santosh.deuja2000@gmail.com>
2023-06-22 17:04:09 +0545

    added signIn part 1

--- Client-App/src/features/auth/components/SignIn.jsx
+++ Client-App/src/features/auth/components/SignIn.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import "../styles/SignIn.css";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className="login-container">
+      <div className="login-sub-container">
+        <h1 className="login-heading">Login</h1>
+        <form className="login-form-holder">
+          {/* Email */}
+          <div className="login-holder">
+            <label className="login-label">E-Mail</label>
+            <div className="login-input-holder">
+              <input type="text" className="login-input" />
+            </div>
+          </div>
+          {/* Password */}
+          <div className="login-holder">
+            <label className="login-label">Password</label>
+            <div className="login-input-holder">
+              <input type="password" className="login-input" />
+              {/* eyeball here */}
+            </div>
+          </div>
+
+          {/*  */}
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
