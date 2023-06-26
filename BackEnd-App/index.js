efb1b1952c7c224bfe9eaec9701da8e5a24631c5
YBD21 <santosh.deuja2000@gmail.com>
2023-06-26 21:54:53 +0545

    added CookieParser --backend

--- BackEnd-App/index.js
+++ BackEnd-App/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser");
 const authSystemRouter = require("./Routes/authSystemRouter.js");
 
 const app = express();
@@ -17,7 +17,11 @@ app.use(
   })
 );
 
+// limit incomming json data to 1 MB
 app.use(express.json({ limit: "1mb" }));
+
+// gives ability to read cookies
+app.use(cookieParser());
 
 // Mount userRouter middleware at "/auth-System" path
 app.use("/auth-system", authSystemRouter);
