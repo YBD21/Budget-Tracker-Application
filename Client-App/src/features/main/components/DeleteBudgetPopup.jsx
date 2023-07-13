f3cfa15bf6d7263ed85cd04d86a2d7aa0240e19e
YBD21 <santosh.deuja2000@gmail.com>
2023-07-13 13:36:06 +0545

    updated DeleteBudgetPopUp Ui --Client

--- Client-App/src/features/main/components/DeleteBudgetPopup.jsx
+++ Client-App/src/features/main/components/DeleteBudgetPopup.jsx
@@ -1,4 +1,4 @@
-import CancelIcon from "@mui/icons-material/Cancel";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 const DeleteBudgetPopup = ({ onChild, deteteData }) => {
   // Type , Amount , Title , Reoccure , Date
@@ -22,14 +22,16 @@ const DeleteBudgetPopup = ({ onChild, deteteData }) => {
     onChild(false);
   };
 
+  const handelDelete = () => {};
+
   return (
     <div className="fixed inset-0 flex justify-center items-center z-50">
       <div className="fixed inset-0 bg-gray-500 opacity-75"></div>
       <div className="relative z-10 inline-block w-full p-6 mx-auto bg-white rounded-lg sm:max-w-2xl sm:p-5 min-h-[20vh]">
         {/* View Single Budget */}
-        <div className="flex flex-col gap-5 text-center">
-          <h3 className="text-black font-semibold text-lg">
-            Are you sure you want to delete this ?
+        <div className="flex flex-col gap-5 text-center p-5">
+          <h3 className="text-black font-semibold text-2xl">
+            Are you sure you want to delete ?
           </h3>
         </div>
 
@@ -85,9 +87,25 @@ const DeleteBudgetPopup = ({ onChild, deteteData }) => {
           </table>
         </div>
 
-        <button className="absolute top-0 right-0 m-5" onClick={close}>
-          <CancelIcon className="svg-icons text-red-900" />
-        </button>
+        <div className="w-full flex justify-between gap-8 mt-8 px-2">
+          <button
+            className="w-1/2 px-5 py-2.5 tracking-wide
+                 bg-red-800  rounded-lg text-center mr-2 mb-2
+           focus:outline-none focus:ring-2 focus:ring-red-800 focus:ring-opacity-50 active:ring-4 active:ring-red-800 active:ring-opacity-50 overflow-hidden"
+            onClick={handelDelete}
+          >
+            <span className="text-white font-semibold text-lg ">Delete</span>
+            <DeleteForeverIcon className="svg-icons text-white ml-6 pb-0.5" />
+          </button>
+          <button
+            className="w-1/2 px-5 py-2.5 tracking-wide
+             text-white bg-neutral-700 font-medium rounded-lg text-center mr-2 mb-2
+             focus:outline-none focus:ring-2 focus:ring-black focus:ring-opacity-50 active:ring-4 active:ring-black active:ring-opacity-50 overflow-hidden"
+            onClick={close}
+          >
+            <span className="text-white font-semibold text-lg">Cancel</span>
+          </button>
+        </div>
       </div>
     </div>
   );
