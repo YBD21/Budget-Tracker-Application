f8d394c5184e61d4172a3d1d6f38c18e8abee8de
YBD21 <santosh.deuja2000@gmail.com>
2023-07-14 15:42:18 +0545

    added fractionDigits Summary --Client

--- Client-App/src/features/main/components/Summary.jsx
+++ Client-App/src/features/main/components/Summary.jsx
@@ -14,31 +14,31 @@ const Summary = () => {
       {/* Total Income */}
       <div className="w-1/3 flex flex-col border-r-2 border-black">
         <p className="text-base font-semibold px-4 max-sm:text-sm">
-          {" "}
-          Total Income{" "}
+          Total Income
         </p>
         <p className="text-lg text-green-900 font-bold px-4 max-sm:text-base max-sm:py-0.5">
-          Rs. {totalIncome}
+          Rs.{" "}
+          {totalIncome.toLocaleString("en-IN", { maximumFractionDigits: 2 })}
         </p>
       </div>
       {/* Total Expense */}
       <div className="w-1/3 flex flex-col">
         <p className="text-base font-semibold px-4 max-sm:text-sm">
-          {" "}
-          Total Expense{" "}
+          Total Expense
         </p>
         <p className="text-lg text-red-800 font-bold px-4 max-sm:text-base max-sm:py-0.5">
-          Rs. {totalExpense}
+          Rs.{" "}
+          {totalExpense.toLocaleString("en-IN", { maximumFractionDigits: 2 })}
         </p>
       </div>
       {/* Balance */}
       <div className="w-1/3 flex flex-col border-l-2 border-black">
         <p className="text-base font-semibold px-4 max-sm:text-sm">
-          {" "}
-          Total Balance{" "}
+          Total Balance
         </p>
         <p className="text-lg text-blue-500 font-bold px-4 max-sm:text-base max-sm:py-0.5">
-          Rs. {totalBalance}
+          Rs.{" "}
+          {totalBalance.toLocaleString("en-IN", { maximumFractionDigits: 2 })}
         </p>
       </div>
     </div>
