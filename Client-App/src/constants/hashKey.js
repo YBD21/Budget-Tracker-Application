355b5bf7d154de1866df853c783232609ac4d68f
YBD21 <santosh.deuja2000@gmail.com>
2023-07-17 10:56:20 +0545

    added reCaptcha ForgotPassword  --Client

--- Client-App/src/constants/hashKey.js
+++ Client-App/src/constants/hashKey.js
@@ -1,8 +1,10 @@
 const secretKey = import.meta.env.VITE_SECRET_KEY;
 const userName = import.meta.env.VITE_USER_NAME;
 const Password = import.meta.env.VITE_PASSWORD;
+const reCaptchaKey = import.meta.env.VITE_RECAPTCHA_KEY;
 export const hashKey = {
   secretKey,
   userName,
   Password,
+  reCaptchaKey,
 };
--- Client-App/src/features/auth/components/ForgotPassword.jsx
+++ Client-App/src/features/auth/components/ForgotPassword.jsx
@@ -1,7 +1,32 @@
-import SearchIcon from "@mui/icons-material/Search";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+import { hashKey } from "../../../constants/hashKey";
+
+import ReCAPTCHA from "react-google-recaptcha";
+import SearchIcon from "@mui/icons-material/Search";
+
 const ForgotPassword = () => {
+  const [recapchaStatus, setRecapchaStatus] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setRecapchaStatus(true);
+  };
+
+  const handleChangeRecapcha = (response) => {
+    // Your code to handle the captcha response
+    console.log(response);
+    
+    // if response call backend to find phone number
+  };
+
+  // useEffect(() => {
+  //   if (recapchaStatus === true) {
+  //     setRecapchaStatus(false);
+  //   }
+  // }, [recapchaStatus]);
+
   return (
     <div className="flex flex-col justify-center min-h-screen">
       <div className="w-full p-6 mb-auto mx-auto mt-32 rounded-md sm:max-w-lg">
@@ -10,10 +35,10 @@ const ForgotPassword = () => {
         </h2>
 
         <p className="text-lg font-semibold text-center text-black mt-3">
-          Enter your email address to find your account.
+          Enter your email address to find your account
         </p>
 
-        <form className="mt-4">
+        <form className="mt-4" onSubmit={handleSubmit}>
           <div className="pb-4">
             <label className="block text-sm font-semibold text-gray-800 py-2">
               Email
@@ -25,28 +50,39 @@ const ForgotPassword = () => {
             />
           </div>
 
-          {/* Search */}
-
-          <div className="min-w-max mt-3">
-            <button
-              className="w-full px-4 py-2.5 tracking-wide
+          {/* ReCapcha */}
+          {recapchaStatus ? (
+            <div className="flex justify-center">
+              <ReCAPTCHA
+                ref={recaptchaRef}
+                sitekey={hashKey.reCaptchaKey}
+                size="normal"
+                onChange={handleChangeRecapcha}
+              />
+            </div>
+          ) : (
+            <div className="min-w-max mt-3">
+              {/* Search */}
+              <button
+                className="w-full px-4 py-2.5 tracking-wide
             text-white bg-black font-medium rounded-lg  text-center mr-3 mb-2
             focus:outline-none focus:ring-2 focus:ring-black focus:ring-opacity-50 active:ring-4 active:ring-black active:ring-opacity-50"
-            >
-              <SearchIcon className="scale-150 mr-4" />
-              Search
-            </button>
-          </div>
+              >
+                <SearchIcon className="scale-150 mr-4" />
+                Search
+              </button>
+            </div>
+          )}
         </form>
 
         <div className="mt-5">
           <p className="text-center text-sm text-gray-800">
-            New here? &nbsp;
+            Return to Login ? &nbsp;
             <Link
               to="/Login"
               className="font-semibold leading-6 text-[#300] hover:underline"
             >
-              Return to login
+              Click Here
             </Link>
           </p>
         </div>
