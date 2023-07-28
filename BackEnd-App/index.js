e6464f722f8537ba170215cf8e76d524dca24f99
YBD21 <santosh.deuja2000@gmail.com>
2023-07-28 12:26:14 +0545

    added security layer --Backend

--- BackEnd-App/index.js
+++ BackEnd-App/index.js
@@ -2,17 +2,38 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const slowDown = require("express-slow-down");
+
 const authSystemRouter = require("./Routes/authSystemRouter.js");
 const budgetSystemRouter = require("./Routes/budgetSystemRouter.js");
 const userManagementSystemRouter = require("./Routes/userManagementSystemRouter.js");
 
 const app = express();
 
+app.use(helmet());
+
+//set proxy
+app.set("trust proxy", 1);
+
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const url = process.env.URL || "http://localhost:3000";
 
-// helmet , express-ratelimit , crsf
+const apiLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 10, // limit each IP to 10 requests per windowMs
+  message: "Too many attempts, please try again later",
+});
+
+const speedLimiter = slowDown({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  delayAfter: 50, // allow 50 requests per 60 minutes, then...
+  delayMs: 500, // begin adding 5ms of delay per request above 50:
+});
+
+app.use(speedLimiter);
 
 //use cors to allow cross origin resource sharing
 app.use(
@@ -29,13 +50,13 @@ app.use(express.json({ limit: "1Mb" }));
 app.use(cookieParser());
 
 // Mount userRouter middleware at "/auth-System" path
-app.use("/auth-system", authSystemRouter);
+app.use("/auth-system", apiLimiter, authSystemRouter);
 
 // Mount userRouter middleware at "/budget-system" path
-app.use("/budget-system", budgetSystemRouter);
+app.use("/budget-system", apiLimiter, budgetSystemRouter);
 
 // Mount userRouter middleware at "/budget-system" path
-app.use("/user-management", userManagementSystemRouter);
+app.use("/user-management", apiLimiter, userManagementSystemRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
